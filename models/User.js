13b4447ef3c0801f1e0b62cc9575c663402cb354
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-09-29 01:55:20 -0400

    create user model/routes

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: 'Username is required!',
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: 'Email is required!',
+    trim: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false
+}
+);
+
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
