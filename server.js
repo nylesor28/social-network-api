d33ef1c90b9a39522a3bf811c152f2626a8fb33e
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-01 22:30:46 -0400

    create indexes on load

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
-
+mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
   useFindAndModify: false,
   useNewUrlParser: true,
